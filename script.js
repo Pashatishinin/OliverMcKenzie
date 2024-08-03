91565e7d8acafddded835d4034e4523c8b1c4404
PaTish <pashatishinin@gmail.com>
2024-08-03 17:42:10 +0200

    contact,skills

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ ScrollReveal({
     reset: false,
 });
 
-ScrollReveal().reveal('.about_img', { origin: 'left'});
-ScrollReveal().reveal('.about_text', { origin: 'right'});
+ScrollReveal().reveal('.about_img,.contact', { origin: 'left'});
+ScrollReveal().reveal('.about_textm,.skills', { origin: 'right'});
 ScrollReveal().reveal('h2, h1', { origin: 'top'});
 ScrollReveal().reveal('.box,.cta-box', { origin: 'bottom'});
 ScrollReveal().reveal('h3', { origin: 'bottom', interval:900});
