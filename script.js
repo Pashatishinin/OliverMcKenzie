c3ed15e4fa18a656b4a34ce7d01d13063dc92c92
PaTish <pashatishinin@gmail.com>
2024-08-03 14:39:11 +0200

    scroll animate

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// project effect 
-$('.wall').jaliswall({
-    item : '.wall-item',
-    columnClass : '.wall-column'
+// scroll reveal
+ScrollReveal({
+    distance:"90px",
+    duration:2000,
+    delay:200,
+    reset: false,
 });
+
+ScrollReveal().reveal('.about_img', { origin: 'left'});
+ScrollReveal().reveal('.about_text', { origin: 'right'});
+ScrollReveal().reveal('h2, h1', { origin: 'top'});
+ScrollReveal().reveal('.box,.cta-box', { origin: 'bottom'});
+ScrollReveal().reveal('h3', { origin: 'bottom', interval:900});
+
