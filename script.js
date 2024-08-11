7ed8b9ad17e29e998ef59f6f1dfc0113c0590989
PaTish <pashatishinin@gmail.com>
2024-08-11 14:42:41 +0200

    menu

--- script.js
+++ script.js
@@ -135,18 +135,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "<",
   );
 
-  tl.to(
-    ".menu-logo",
-    {
-      duration: 1,
-      height: "200px",
-      ease: "power2.out",
-    },
-    "<",
-  );
-
-  
-
   function openMenu() {
     document.querySelector(".menu-overlay").style.pointerEvents = "all";
     tl.play();
@@ -157,15 +145,71 @@ document.addEventListener("DOMContentLoaded", function () {
     tl.reverse();
   }
 
-  document.querySelector(".menu-open-btn").addEventListener("click", openMenu);
-  document
-    .querySelector(".menu-close-btn")
-    .addEventListener("click", closeMenu);
-    document.querySelectorAll('.link_click').forEach(link => {
-      link.addEventListener("click", closeMenu);
+  document.querySelector(".menu-open-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    openMenu();
+  });
+
+  document.querySelector(".menu-close-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    closeMenu();
+  });
+
+  document.querySelectorAll('.link_click').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      closeMenu();
     });
-  tl.reverse();
+  });
+
+  tl.reverse(); // Устанавливаем начальное состояние меню закрытым
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let tl = gsap.timeline({ paused: true });
+
+//   tl.to(".menu-overlay", {
+//     duration: 1,
+//     clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+//     ease: "power2.out",
+//   });
+
+//   tl.from(
+//     ".menu-link, .btn",
+//     {
+//       opacity: 0,
+//       y: 60,
+//       stagger: 0.05,
+//       duration: 0.75,
+//       ease: "power1.inOut",
+//     },
+//     "<",
+//   );
+
+  
+
+  
+
+//   function openMenu() {
+//     document.querySelector(".menu-overlay").style.pointerEvents = "all";
+//     tl.play();
+//   }
+
+//   function closeMenu() {
+//     document.querySelector(".menu-overlay").style.pointerEvents = "none";
+//     tl.reverse();
+//   }
+
+//   document.querySelector(".menu-open-btn").addEventListener("click", openMenu);
+//   document.querySelector(".menu-close-btn").addEventListener("click", closeMenu);
+//     document.querySelectorAll('.link_click').forEach(link => {
+//       link.addEventListener("click", closeMenu);
+//     });
+//   tl.reverse();
+// });
 
 
 
