ed3bdd74b95006992d13d9da0400af3ba1ef5e8e
PaTish <pashatishinin@gmail.com>
2024-08-11 20:56:07 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleButton.addEventListener("click", toggleMenu);
 
   // Обработчик клика на ссылки внутри меню
-  document.querySelectorAll('.menu-item-name a').forEach(link => {
+  document.querySelectorAll('.menu-item-name a, .left_header').forEach(link => {
     link.addEventListener("click", function (event) {
       event.preventDefault(); // Предотвращаем стандартное действие ссылки
       toggleMenu(); // Переключаем состояние меню
@@ -180,52 +180,7 @@ if (Wall) {
 
 
 
-//menu 
-//GSAP
 
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let tl = gsap.timeline({ paused: true });
-
-//   tl.to(".menu-overlay", {
-//     duration: 1,
-//     clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-//     ease: "power2.out",
-//   });
-
-//   tl.from(
-//     ".menu-link, .btn",
-//     {
-//       opacity: 0,
-//       y: 60,
-//       stagger: 0.05,
-//       duration: 0.75,
-//       ease: "power1.inOut",
-//     },
-//     "<",
-//   );
-
-  
-
-  
-
-//   function openMenu() {
-//     document.querySelector(".menu-overlay").style.pointerEvents = "all";
-//     tl.play();
-//   }
-
-//   function closeMenu() {
-//     document.querySelector(".menu-overlay").style.pointerEvents = "none";
-//     tl.reverse();
-//   }
-
-//   document.querySelector(".menu-open-btn").addEventListener("click", openMenu);
-//   document.querySelector(".menu-close-btn").addEventListener("click", closeMenu);
-//     document.querySelectorAll('.link_click').forEach(link => {
-//       link.addEventListener("click", closeMenu);
-//     });
-//   tl.reverse();
-// });
 
 
 
